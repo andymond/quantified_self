768dad24ff2441f5441fa3b85411f13380b80bb5
Andrew Dymond <andy.dymond@gmail.com>
2018-04-30 15:03:35 -0600

    Add params validation & create action to food_controller, add create to routes

--- app/controllers/api/v1/foods_controller.rb
+++ app/controllers/api/v1/foods_controller.rb
@@ -1,4 +1,5 @@
 class Api::V1::FoodsController < ApplicationController
+  before_action :validate_params, only: :create
 
   def index
     render json: Food.all
@@ -7,5 +8,23 @@ class Api::V1::FoodsController < ApplicationController
   def show
     render json: Food.find(params[:id])
   end
+
+  def create
+    food = Food.create(food_params)
+    render json: Food.find(food.id)
+  end
+
+  private
+
+    def food_params
+      params.require(:food).permit(:name, :calories)
+    end
+
+    def validate_params
+      payload = {error: "missing param"}
+      if food_params[:name] === "" || food_params[:calories] === ""
+        render json: payload, status: 400
+      end
+    end
 
 end
