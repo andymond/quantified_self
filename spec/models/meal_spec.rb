09d9eefe8b0871ec021e4882a5acc7f4b67edda6
Andrew Dymond <andy.dymond@gmail.com>
2018-04-30 16:41:07 -0600

    Add validations & relationships to meals & foods

--- spec/models/meal_spec.rb
+++ spec/models/meal_spec.rb
@@ -3,5 +3,5 @@ require 'rails_helper'
 describe Meal do
   it {should validate_presence_of(:name)}
   it {should have_many(:meal_foods)}
-  it {should have_many(:meals)}
+  it {should have_many(:foods)}
 end
