d47548ab720b2e1d0f1bf3814419533eab720f53
Andrew Dymond <andy.dymond@gmail.com>
2018-04-30 14:33:18 -0600

    Add it "can return a single food by id" spec to foods_spec

--- spec/requests/api/v1/food/foods_spec.rb
+++ spec/requests/api/v1/food/foods_spec.rb
@@ -4,16 +4,31 @@ describe "API/V1" do
   it "can return index of all existing foods" do
     create_list(:food, 10)
 
-    get '/api/v1/foods'
+    get "/api/v1/foods"
 
     expect(response).to be_success
 
     results = JSON.parse(response.body, symbolize_names: true)
 
+    expect(results.count).to be_an Array
     expect(results.count).to eq(10)
     expect(results.first[:name]).to eq('testfood1')
     expect(results.first[:calories]).to eq(100)
     expect(results.last[:name]).to eq('testfood10')
     expect(results.last[:calories]).to eq(1000)
+  end
+
+  it "can return a single food by id" do
+    food_1 = create(:food)
+
+    get "/api/v1/foods/#{food_1.id}"
+
+    expect(response).to be_success
+
+    results = JSON.parse(response.body, symbolize_names: true)
+
+    expect(results).to be_a Hash
+    expect(results[:name]).to eq(food_1.name)
+    expect(results[:calories]).to eq(food_2.name)
   end
 end
