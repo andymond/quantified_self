c18f25d80e164f4e952045c0e05e0dfc62258b97
Andrew Dymond <andy.dymond@gmail.com>
2018-04-30 14:25:58 -0600

    Add api/v1/foods route & corresponding controller, spec passing

--- spec/factories/foods.rb
+++ spec/factories/foods.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :food do
-    sequence(:name) { |n| "testfood#{n}"}
-    sequence(:calories) { |n| "testfood#{n * 100}"}
+    sequence(:name) { |n| "testfood#{n}" }
+    sequence(:calories) { |n| n * 100 }
   end
 end
--- spec/requests/api/v1/food/foods_spec.rb
+++ spec/requests/api/v1/food/foods_spec.rb
@@ -4,14 +4,16 @@ describe "API/V1" do
   it "can return index of all existing foods" do
     create_list(:food, 10)
 
-    get 'api/v1/foods'
+    get '/api/v1/foods'
 
-    results = JSON.parse(response, symbolize_names: true)
+    expect(response).to be_success
+
+    results = JSON.parse(response.body, symbolize_names: true)
 
     expect(results.count).to eq(10)
-    expect(results.first.name).to eq('testfood1')
-    expect(results.first.calories).to eq('100')
-    expect(results.last.calories).to eq('testfood10')
-    expect(results.last.calories).to eq('1000')
+    expect(results.first[:name]).to eq('testfood1')
+    expect(results.first[:calories]).to eq(100)
+    expect(results.last[:name]).to eq('testfood10')
+    expect(results.last[:calories]).to eq(1000)
   end
 end
