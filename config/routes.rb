dcd7ca8878e65eafaa4a441c0635464c361af344
Andrew Dymond <andy.dymond@gmail.com>
2018-05-01 13:53:33 -0600

    Add show action to meals_controller & corresponding route

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :foods, only: [:index, :show, :create, :update, :destroy]
-      resources :meals, only: [:index]
+      resources :meals, only: [:index, :show]
     end
   end
 end
